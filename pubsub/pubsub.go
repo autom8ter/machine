e725ef191a6080b333623f6fa6c5ed9866e7868c
autom8ter <colemanword@gmail.com>
2021-02-18 23:19:30 -0700

    add glob match for pubsub subscriptions

--- pubsub/pubsub.go
+++ pubsub/pubsub.go
@@ -3,6 +3,7 @@ package pubsub
 import (
 	"context"
 	"math/rand"
+	"strings"
 	"sync"
 )
 
@@ -17,28 +18,40 @@ type PubSub interface {
 	// Publish publishes the object to the channel by name
 	Publish(channel string, obj interface{}) error
 	// Subscribe subscribes to the given channel until the context is cancelled.
-	Subscribe(ctx context.Context, channel string, handler Handler) error
-	// SubscribeFilter subscribes to messages that pass a given filter
-	SubscribeFilter(ctx context.Context, channel string, filter Filter, handler Handler) error
+	Subscribe(ctx context.Context, channel string, handler Handler, opts ...SubOpt) error
 	// Close closes all subscriptions
 	Close()
 }
 
+type SubOptions struct {
+	filter Filter
+}
+
+type SubOpt func(options *SubOptions)
+
+func WithFilter(filter Filter) SubOpt {
+	return func(options *SubOptions) {
+		options.filter = filter
+	}
+}
+
 type pubSub struct {
-	subscriptions    map[string]map[int]chan interface{}
-	gobSubscriptions map[string]map[int]chan interface{}
-	subMu            sync.RWMutex
+	subscriptions map[string]map[int]chan interface{}
+	subMu         sync.RWMutex
 }
 
 func NewPubSub() PubSub {
 	return &pubSub{
-		subscriptions:    map[string]map[int]chan interface{}{},
-		gobSubscriptions: nil,
-		subMu:            sync.RWMutex{},
+		subscriptions: map[string]map[int]chan interface{}{},
+		subMu:         sync.RWMutex{},
 	}
 }
 
-func (p *pubSub) Subscribe(ctx context.Context, channel string, handler Handler) error {
+func (p *pubSub) Subscribe(ctx context.Context, channel string, handler Handler, options ...SubOpt) error {
+	opts := &SubOptions{}
+	for _, o := range options {
+		o(opts)
+	}
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 	ch, closer := p.setupSubscription(channel)
@@ -48,27 +61,11 @@ func (p *pubSub) Subscribe(ctx context.Context, channel string, handler Handler)
 		case <-ctx.Done():
 			return nil
 		case msg := <-ch:
+			if opts.filter != nil && !opts.filter(msg) {
+				continue
+			}
 			if !handler(msg) {
 				return nil
-			}
-		}
-	}
-}
-
-func (p *pubSub) SubscribeFilter(ctx context.Context, channel string, decider Filter, handler Handler) error {
-	ctx, cancel := context.WithCancel(ctx)
-	defer cancel()
-	ch, closer := p.setupSubscription(channel)
-	defer closer()
-	for {
-		select {
-		case <-ctx.Done():
-			return nil
-		case msg := <-ch:
-			if decider(msg) {
-				if !handler(msg) {
-					return nil
-				}
 			}
 		}
 	}
@@ -77,13 +74,11 @@ func (p *pubSub) SubscribeFilter(ctx context.Context, channel string, decider Fi
 func (p *pubSub) Publish(channel string, obj interface{}) error {
 	p.subMu.RLock()
 	defer p.subMu.RUnlock()
-	if p.subscriptions[channel] == nil {
-		return nil
-	}
-	channelSubscribers := p.subscriptions[channel]
-	if channelSubscribers != nil {
-		for _, ch := range channelSubscribers {
-			ch <- obj
+	for k, channelSubscribers := range p.subscriptions {
+		if globMatch(k, channel) {
+			for _, ch := range channelSubscribers {
+				ch <- obj
+			}
 		}
 	}
 	return nil
@@ -91,7 +86,7 @@ func (p *pubSub) Publish(channel string, obj interface{}) error {
 
 func (p *pubSub) setupSubscription(channel string) (chan interface{}, func()) {
 	subId := rand.Int()
-	ch := make(chan interface{}, 1)
+	ch := make(chan interface{}, 10)
 	p.subMu.Lock()
 	if p.subscriptions[channel] == nil {
 		p.subscriptions[channel] = map[int]chan interface{}{}
@@ -108,8 +103,42 @@ func (p *pubSub) setupSubscription(channel string) (chan interface{}, func()) {
 
 func (p *pubSub) Close() {
 	p.subMu.Lock()
+	defer p.subMu.Unlock()
 	for k, _ := range p.subscriptions {
 		delete(p.subscriptions, k)
 	}
-	p.subMu.Unlock()
+}
+
+func globMatch(pattern string, subj string) bool {
+	const matchAll = "*"
+	if pattern == subj {
+		return true
+	}
+	if pattern == matchAll {
+		return true
+	}
+
+	parts := strings.Split(pattern, matchAll)
+	if len(parts) == 1 {
+		return subj == pattern
+	}
+	leadingGlob := strings.HasPrefix(pattern, matchAll)
+	trailingGlob := strings.HasSuffix(pattern, matchAll)
+	end := len(parts) - 1
+	for i := 0; i < end; i++ {
+		idx := strings.Index(subj, parts[i])
+
+		switch i {
+		case 0:
+			if !leadingGlob && idx != 0 {
+				return false
+			}
+		default:
+			if idx < 0 {
+				return false
+			}
+		}
+		subj = subj[idx+len(parts[i]):]
+	}
+	return trailingGlob || strings.HasSuffix(subj, parts[end])
 }
